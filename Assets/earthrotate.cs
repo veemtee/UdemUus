270bfb394b49620693a85f1f09428cf3a1335bc7
veemtee <veemtee@protonmail.com>
2021-05-14 10:08:14 +0300

    Projekti kopioitu uudeki gitiksi

---
+++ Assets/earthrotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class earthrotate : MonoBehaviour
+{
+    public float rotate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * rotate * Time.deltaTime);
+        //transform.Translate(Vector3.up * 2 * Time.deltaTime);
+    }
+}
